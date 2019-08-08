463e50a358a1c184776c09072d3a2074462d113b
Nadeem Shah <nadeem.abbas.shah90@gmail.com>
2019-08-09 03:38:29 +0500

    Task Event Listener added

---
+++ app/Listeners/TaskExecutedListener.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\TaskExecutedEvent;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class TaskExecutedListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  TaskExecutedEvent  $event
+     * @return void
+     */
+    public function handle(TaskExecutedEvent $event)
+    {
+        if (file_exists(storage_path('task-'.sha1($event->task->command . $event->task->expression)))) {
+            $output = file_get_contents(storage_path('task-'.sha1($event->task->command . $event->task->expression)));
+            $event->task->results()->create([
+                'duration' => $event->elapsed_time * 1000,
+                'result' => $output
+            ]);
+            unlink(storage_path('task-'.sha1($event->task->command . $event->task->expression)));
+        }
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Console\Scheduling\Schedule;
 use App\Observers\TaskObserver;
 use App\Task;
+use App\Events\TaskExecutedEvent;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,37 +24,7 @@ class AppServiceProvider extends ServiceProvider
 
         if (Schema::hasTable('tasks')) {
             $this->app->resolving(Schedule::class, function ($schedule) {
-                // Fetch all the active tasks
-                $tasks = app('App\Task')->getActive();
-                // $tasks = app('App\Task')->where('is_active', true)->get();
-                //schedule the tasks
-                foreach ($tasks as $task) {
-                    $event = $schedule->exec($task->command);
-                    $event->cron($task->expression)
-                ->before(function () use ($event) {
-                    $event->start = microtime(true);
-                })
-                ->sendOutputTo(storage_path('task-'.sha1($task->command . $task->expression)))
-                ->after(function () use ($event,$task) {
-                    $elapsed_time = microtime(true) - $event->start;
-                    if (file_exists(storage_path('task-'.sha1($task->command . $task->expression)))) {
-                        $output = file_get_contents(storage_path('task-'.sha1($task->command . $task->expression)));
-                        $task->results()->create([
-                            'duration' => $elapsed_time * 1000,
-                            'result' => $output
-                        ]);
-                        unlink(storage_path('task-'.sha1($task->command . $task->expression)));
-                    }
-                });
-
-                    if ($task->dont_overlap) {
-                        $event->withoutOverlapping();
-                    }
-
-                    if ($task->run_in_maintenance){
-                        $event->evenInMaintenanceMode();
-                    }
-                }
+                $this->schedule($schedule);
             });
         }
     }
@@ -66,5 +37,33 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+    }
+
+    public function schedule(Schedule $schedule)
+    {
+        // Fetch all the active tasks
+        $tasks = app('App\Task')->getActive();
+        // $tasks = app('App\Task')->where('is_active', true)->get();
+        //schedule the tasks
+        foreach ($tasks as $task) {
+            $event = $schedule->exec($task->command);
+            $event->cron($task->expression)
+        ->before(function () use ($event) {
+            $event->start = microtime(true);
+        })
+        ->sendOutputTo(storage_path('task-'.sha1($task->command . $task->expression)))
+        ->after(function () use ($event,$task) {
+            $elapsed_time = microtime(true) - $event->start;
+            event(new TaskExecutedEvent($task,$elapsed_time));
+        });
+
+            if ($task->dont_overlap) {
+                $event->withoutOverlapping();
+            }
+
+            if ($task->run_in_maintenance){
+                $event->evenInMaintenanceMode();
+            }
+        }
     }
 }
